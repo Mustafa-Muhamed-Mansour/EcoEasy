11bec86bc69dbb8c7e13c2b8dab45b1391ab6478
Mustafa-Muhamed-Mansour <mostafamansour163@yahoo.com>
2025-05-13 19:42:18 +0300

    enhancement file "UiState" meaning i removed data classes ara that used

--- data/src/commonMain/kotlin/com/eco_easy/data/utils/UiStates.kt
+++ data/src/commonMain/kotlin/com/eco_easy/data/utils/UiStates.kt
@@ -3,13 +3,6 @@ package com.eco_easy.data.utils
 import com.eco_easy.domain.response.CategoryResponse
 import com.eco_easy.domain.response.ProductResponse
 
-
-//data class UiStateDragons(
-//    val isLoading: Boolean = false,
-//    val error: String = "",
-//    val data: List<DragonModel>? = null
-//)
-
 data class UiStateProducts(
     val isLoading: Boolean = false,
     val error: String = "",
@@ -21,17 +14,3 @@ data class UiStateCategories(
     val error: String = "",
     val data: CategoryResponse ?= null
 )
-
-
-//data class UiStateDragonDetails(
-//    val isLoading: Boolean = false,
-//    val error: String = "",
-//    val data: CharacterResponseById? = null
-//)
-//
-//
-//data class UiStateSearchDragons(
-//    val isLoading: Boolean = false,
-//    val error: String = "",
-//    val data: List<SearchDragonsResponseItem>? = null
-//)
